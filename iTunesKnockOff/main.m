45f6136cbf64c10e275d230f7d090b83ef135ad3
ANUJ DESHMUKH <anuj.deshmukh@student.csulb.edu>
2014-09-23 15:12:51 -0700

    Initial Commit

---
+++ iTunesKnockOff/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iTunesKnockOff
+//
+//  Created by ANUJ DESHMUKH on 9/23/14.
+//  Copyright (c) 2014 DESHMUKH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
